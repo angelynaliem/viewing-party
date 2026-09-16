ce3935c75a1c9c511e26ed4c09e308d87f6286c5
Angelyn Liem <angelynliem@gmail.com>
2026-09-15 22:41:27 -0400

    passed all tests in wave 4 and 5, but need to refactor and add the assertions in the tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -170,12 +170,123 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    """
+    user_data is a dictionary with a "watched" list of movie dictionaries, and a "friends" list
+    each item in the friends list is a dictionary
+    each movie dictionary has a "title"
+    Return a list of dictionaries that represents a list of movies that the user has watched, but the friends have not.
+    
+    user_data = {
+    
+    "watched" : [{"title" : "Finding Nemo}, {"title" : "Wicked"}],
+    
+    "friends" : [
+    {
+    "watched" : [{"title" : "Wicked"}, {"title" : "Mulan"}]
+    }
+    ]
+
+    }
+    """
+
+    unique_movies = []
+    user_unique_movies = []
+    friends_unique_movies = []
+
+    for movie in user_data["watched"]:
+        if "title" in movie:
+            user_unique_movies.append(movie)
+
+    for friends in user_data["friends"]:
+        for friend in friends["watched"]:
+            if "title" in friend:
+                friends_unique_movies.append(friend)
+
+    for user_movie in user_unique_movies:
+        if user_movie not in friends_unique_movies:
+            unique_movies.append(user_movie)
+
+    return unique_movies
+    
+            
+def get_friends_unique_watched(user_data):
+    """
+    user_data has the same structure as previous function
+    find out a movie in the friends list that the user has not watched
+    return a list of movies that the user has not watched that the friend/s have
+    """
+    unique_movies = []
+    user_unique_movies = []
+    friends_unique_movies = []
+    
+    for movie in user_data["watched"]:
+        if "title" in movie:
+            user_unique_movies.append(movie)
+    
+    for friends in user_data["friends"]:
+        for friend in friends["watched"]:
+            if "title" in friend:
+                friends_unique_movies.append(friend)
+    
+    for friend_movie in friends_unique_movies:
+        if friend_movie not in user_unique_movies:
+            if friend_movie not in unique_movies:
+                unique_movies.append(friend_movie)
+    
+    return unique_movies
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data):
+    """
+    user_data has a key "subscriptions"
+    the value of "subscriptions" is a list of strings
+    for each friend, each movie has a key "host"
+    the value of "host" is a string
+
+    return a list of recommended movies
+    criteria for the recommended movie:
+    - user has not watched it
+    - a friend has watched it
+    - the "host" of the movie is a service in the user's "subscriptions"
+
+    user_data = {
+        
+        "watched" : [{"title" : "Finding Nemo}, {"title" : "Wicked"}],
+        
+        "friends" : [
+        {
+        "watched" : [{"title" : "Wicked", "host" : "amazon prime"}, {"title" : "Mulan", "host" : "netflix"}]
+        }
+        ],
+
+        "subscriptions" : ["netflix", "amazon prime"]
+    
+        }
+
+    Step 1 - Create a list for recommended movies
+    Step 2 - Iterate through the each friend's watched movies list to see if the user has not watched any of the movies.
+    Step 3 - Check if the movie's host exists in the user's subscriptions. If so, add that movie to the recommended movies list
+    """
+
+    recommended_movies = []
+    for friends in user_data["friends"]:
+        for friend in friends["watched"]:
+            if friend not in user_data["watched"]:
+                if friend["host"] in user_data["subscriptions"]:
+                    if friend not in recommended_movies:
+                        recommended_movies.append(friend)
+
+    return recommended_movies
+
+
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+
+
 
