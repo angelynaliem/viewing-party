59af4d06b0364ae2b75a958738c68262bba36edd
Tatsiana Khurs <tatsianakhurs@gmail.com>
2026-09-24 22:51:59 -0700

    Updated Wave3 tests test_friends_unique_movies_not_duplicated

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -57,6 +57,8 @@ def test_friends_unique_movies_not_duplicated():
 
     # Checks that the correct movies are in friends_unique_movies
     assert INTRIGUE_3 in friends_unique_movies
+    assert HORROR_1 in friends_unique_movies
+    assert FANTASY_4 in friends_unique_movies
 
 # @pytest.mark.skip()
 def test_friends_not_unique_movies():
